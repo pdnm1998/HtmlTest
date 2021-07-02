32aeaefeedd6848baf631f4884756291a0ae55a7
nhatminh <nkocvjp92@gmail.com>
2021-07-02 17:00:10 +0700

    buoi2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var counter = 1
-setInterval(function() {
-    document.getElementById('radio' + counter).checked = true;
-    counter++
-    if (counter > 4) {
-        counter = 1
+// var counter = 1;
+// setInterval(function() {
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
+//     if (counter > 4) {
+//         counter = 1;
 
-    }
-}, 5000)
+//     }
+// }, 5000)
