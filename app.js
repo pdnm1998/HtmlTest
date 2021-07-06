d09249fb8ab73b8c33e77db6195ba7f35cb81ccf
nhatminh <nkocvjp92@gmail.com>
2021-07-06 11:37:44 +0700

    demo

--- app.js
+++ app.js
@@ -19,3 +19,12 @@ $(document).ready(function() {
 $('#sandbox-container input').datepicker({
     todayHighlight: true
 });
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+});
